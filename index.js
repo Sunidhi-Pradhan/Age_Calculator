1683eec12142819e47331abf7cf04491b67e6924
Sunidhi-Pradhan <sunidhipradhan16@gmail.com>
2025-04-18 08:46:58 +0530

    modify

--- index.js
+++ index.js
@@ -8,19 +8,22 @@ function calculateAge() {
     let d1 = birthDate.getDate();
     let m1 = birthDate.getMonth() + 1; // Months are zero-based
     let y1 = birthDate.getFullYear();
+
     let today = new Date();
-    let d2 = new Date().getDate();
-    let m2 = new Date().getMonth() + 1; // Months are zero-based
-    let y2 = new Date().getFullYear();
+
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1; // Months are zero-based
+    let y2 = today.getFullYear();
 
     let d3, m3, y3;
+    // Calculate the difference in years
     y3 = y2 - y1;
 
     if (m2 > m1) {
         m3 = m2 - m1;
     } else {
         y3--;
-        m3 = (12 + m2) - m1;
+        m3 = 12 + m2 - m1;
     }
     if (d2 >= d1) {
         d3 = d2 - d1;
@@ -33,8 +36,8 @@ function calculateAge() {
         y3--;
     }
 
-    result.innerHTML = `you are <span>${y3}</span> years ,<span>${m3}</span> months and <span>${d3}</span> days old`
-    console.log(y3,m3,d3);
+    result.innerHTML = `you are  <span> ${y3}</span> years ,<span>${m3}</span> months and <span> ${d3}</span> days old`
+    console.log(y3, m3, d3);
 
     function getDaysInMonth(year, month) {
         return new Date(year, month, 0).getDate();
